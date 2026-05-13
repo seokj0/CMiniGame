5ac73686234809fc00942f30fd1576723a4a7a8b
seokju <sj200407121124@gmail.com>
2026-05-13 21:33:37 +0900

    메인화면 개발 완료하였습니다.

--- tetris_model.h
+++ tetris_model.h
@@ -1,1 +1,10 @@
-#pragma once
+#ifndef TETRIS_MODEL_H
+#define TETRIS_MODEL_H
+
+#define BOARD_WIDTH 10
+#define BOARD_HEIGHT 20
+
+// 맵 데이터 (0: 빈공간, 1: 벽, 2: 쌓인 블록)
+extern int g_board[BOARD_HEIGHT][BOARD_WIDTH];
+
+#endif
